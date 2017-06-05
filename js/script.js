3250fb7c43d8aea4f0b81992db40cb49bf854f09
AlexGreenvi <alexgreenvi@yandex.ru>
2017-06-05 11:28:53 +0300

    Верстка # Купить в один клик

--- js/script.js
+++ js/script.js
@@ -749,6 +749,21 @@
 						enabled: false
 					}
 				});
+                $('.popup-inline').magnificPopup({
+                    type: 'inline',
+
+                    fixedContentPos: false,
+                    fixedBgPos: true,
+
+                    overflowY: 'auto',
+
+                    closeBtnInside: true,
+                    preloader: false,
+
+                    midClick: true,
+                    removalDelay: 300,
+                    mainClass: 'my-mfp-zoom-in'
+                });
 			}
 		},
 		navDropdown: function(){
