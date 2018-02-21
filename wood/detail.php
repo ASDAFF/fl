22d33ba41d0021ea65440c8e7360c6cc2ee8f4c5
Prod <prod@veleslife.ru>
2018-02-21 17:15:26 +0300

    prod

--- wood/detail.php
+++
@@ -1,7 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Производители");
-
-$APPLICATION->IncludeComponent('tim:empty', 'wood.detail');
-
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
